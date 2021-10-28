657c1f66d40227c3277b2d9e25463e40ee5fe67d
Abhishek SIngh <abhishekmithileshsingh1@gmail.com>
2021-10-28 21:36:31 +0530

    proper naming of constant and use of constexpr

--- Constants.cpp
+++ Constants.cpp
@@ -34,7 +34,25 @@ void constant_basics() {
     const bool cmp = ci1 < pi;
     std::cout << " cmp = " << cmp << std::endl;
     std::cout << "cmp ="<< std::setiosflags(std::ios::boolalpha) << cmp << std::endl;
-    
+    int a;
+    int b;
+    std::cout << "Enter Two integer =";
+    std::cin >> a >> b;
+    const bool flag = a < b;
+    std::cout << " flsg = " << flag << std::endl;
+    std::cout << " Flag =" << std::setiosflags(std::ios::boolalpha) << flag << std::endl;
+    constexpr int max = 8 * 1024;//compile time constant
+    std::cout << "Max :" << max << std::endl;
+    constexpr int min = 10;
+    std::cout << " min : " << min << std::endl;
+    constexpr bool flag1 = min < max;
+    std::cout << " flag1 :" << flag1 << std::endl;
+    //constexpr int min = a * b; // run time constant 
+    const int product  = a * b;
+    std::cout << "product :" << product << std :: endl;
+
+
+
 
 
     //A Constexpr VAriable accepts only compile - time value 
@@ -45,8 +63,26 @@ void constant_basics() {
 
 
 void constant_use_case1() {
+    int age;
+    std::cout << "What's your  age (years)" << std::endl;
+    std::cin >> age;
+    if (age >= 18) {
+        std::cout << "you are eligible to vote " << std::endl;
+}
+    else {
+        std::cout << "your are not eligible to vote " << std::endl;
+    }
 
+    //Avoid Magic constant use symbolic constants
+    constexpr int MIN_AGE_YEAR_FOR_VOTE = 18;
+    std::cout << " what's your age (years) ?" << std::endl;
+    if (age >= MIN_AGE_YEAR_FOR_VOTE) {
+        std::cout << "you are eligible to vote! " << std::endl;
 
+    }
+    else {
+        std::cout << " you are not eligible to vote"
+    }
 }
 
 
